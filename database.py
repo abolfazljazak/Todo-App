6b8efc986be4d4df4d870396da398c7900b95e40
abolfazl jazak <jazakabolfazl@gmail.com>
2024-08-09 18:15:06 +0330

    feat(main,-database): added get_db and refactored main, database

--- database.py
+++ database.py
@@ -1,12 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext import declarative
+from sqlalchemy.ext.declarative import declarative_base
 
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos-db'
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_some_thears': False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
-SessionLocal = sessionmaker(autocommit=False, autoflash=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative()
+Base = declarative_base()
--- main.py
+++ main.py
@@ -1,8 +1,34 @@
-from fastapi import FastAPI
+from fastapi import (
+    FastAPI,
+    Depends
+)
 
+from database import (
+    engine,
+    SessionLocal
+)
+
+from models import (
+    Base,
+    Todos,
+)
+
+from sqlalchemy.orm import Session
+from typing import Annotated
 
 app = FastAPI()
 
-@app.get('/home')
-def home():
-    return {'message': 'Welcome to the Home Page'}
+Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get('/')
+async def read_all(db: Annotated[Session, Depends(get_db)]):
+    return db.query(Todos).all()
