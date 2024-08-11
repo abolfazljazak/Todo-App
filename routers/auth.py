5877c3e5baaf8ea03b3be78a2905a3cb21801f80
abolfazl jazak <jazakabolfazl@gmail.com>
2024-08-11 19:08:18 +0330

    feat(auth): added hash password with passlib, bcrypt

--- routers/auth.py
+++ routers/auth.py
@@ -1,10 +1,33 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from passlib.context import CryptContext
 
+from models import Users
 
 router = APIRouter()
 
 
-@router.get("/auth/")
-async def get_user():
-    return {'user': "authenticated"}
+bcrypt_context = CryptContext(schemes='bcrypt', deprecated="auto")
 
+
+class CreateUserRequest(BaseModel):
+    username: str
+    password: str
+    email: str
+    first_name: str
+    last_name: str
+    role: str
+
+
+@router.post("/auth/")
+async def get_user(create_user_request: CreateUserRequest):
+
+    user = Users(
+        username=create_user_request.username,
+        hash_password=bcrypt_context.hash(create_user_request.password),
+        email=create_user_request.email,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        role=create_user_request.role,
+    )
+    return user
