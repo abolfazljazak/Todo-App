9d0cb0c3efa70b05ab018bbe52b7183e49def311
abolfazl jazak <jazakabolfazl@gmail.com>
2024-08-11 18:28:06 +0330

    feat(models): added table users and added user-fk to model todo

--- models.py
+++ models.py
@@ -3,8 +3,22 @@ from sqlalchemy import (
     Column,
     Integer,
     String,
-    Boolean
+    Boolean,
+    ForeignKey
 )
+
+
+class Users(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hash_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
 
 
 class Todos(Base):
@@ -15,3 +29,4 @@ class Todos(Base):
     description = Column(String)
     priority = Column(Integer)
     completed = Column(Boolean, default=False)
+    owner = Column(Integer, ForeignKey("users.id"))
